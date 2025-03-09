9ca8f6133fd2c416ccbe36aa3dc70e6b9f3182d3
Tay Ahuva <ttay29@gmail.com>
2025-03-09 15:50:14 +0200

    Final addition to laser puzzle and started dice puzzle

--- Assets/Scripts/PlayerStateControl.cs
+++ Assets/Scripts/PlayerStateControl.cs
@@ -1,84 +1,110 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerStateControl : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject playerGameObject;
-    
-    [SerializeField]
-    private GameObject playerCamera;
-
-    [SerializeField]
-    private GameObject laserPuzzleObject;
-    
-    [SerializeField]
-    private GameObject laserPuzzleCamera;
-
-    [SerializeField] 
-    private Animator fadeAnimator;
+    [System.Serializable]
+    public class PuzzleSetup
+    {
+        public string puzzleName;
+        public GameObject puzzleObject;
+        public GameObject puzzleCamera;
+        public PlayerState stateType;
+    }
 
     public enum PlayerState
     {
         Moving,
         LaserPuzzle,
         CubePuzzle,
+        // Add more puzzle states here
     }
 
+    [SerializeField] private GameObject playerGameObject;
+    [SerializeField] private GameObject playerCamera;
+    [SerializeField] private Animator fadeAnimator;
+    [SerializeField] private float fadeInDuration = 1f;
+    [SerializeField] private float transitionDuration = 0.5f;
+    [SerializeField] private List<PuzzleSetup> puzzles = new List<PuzzleSetup>();
+
+    private Dictionary<PlayerState, PuzzleSetup> puzzleDict = new Dictionary<PlayerState, PuzzleSetup>();
     public PlayerState currentState = PlayerState.Moving;
+    private PuzzleSetup currentActivePuzzle;
 
     private void Awake()
     {
+        // Initialize dictionary for faster lookups
+        foreach (var puzzle in puzzles)
+        {
+            puzzleDict[puzzle.stateType] = puzzle;
+            puzzle.puzzleObject.SetActive(false);
+        }
+
+        // Initial state setup
         playerGameObject.SetActive(true);
-        laserPuzzleObject.SetActive(false);
+        playerCamera.SetActive(true);
     }
 
-    private IEnumerator SwitchCamera(string cameraName)
+    private IEnumerator TransitionToState(PlayerState newState)
     {
-       
-        if (cameraName == "Player")
+        // Start fade animation
+        fadeAnimator.SetBool("isFinished", false);
+        fadeAnimator.SetTrigger("StartFade");
+        yield return new WaitForSeconds(fadeInDuration);
+        
+        // Handle the transition based on target state
+        if (newState == PlayerState.Moving)
         {
-            fadeAnimator.SetBool("isFinished", false);
-            fadeAnimator.SetTrigger("StartFade");
-            yield return new WaitForSeconds(1f);
-           //Turn Off/On camera 
+            // Disable current puzzle if there is one
+            if (currentActivePuzzle != null)
+            {
+                currentActivePuzzle.puzzleCamera.SetActive(false);
+                currentActivePuzzle.puzzleObject.SetActive(false);
+                currentActivePuzzle = null;
+            }
+            
+            // Enable player
             playerCamera.SetActive(true);
-            laserPuzzleCamera.SetActive(false);
-            yield return new WaitForSeconds(0.5f);
-            //Turn Off/On the gameobject 
+            yield return new WaitForSeconds(transitionDuration);
             playerGameObject.SetActive(true);
-            laserPuzzleObject.SetActive(false);
-            yield return new WaitForSeconds(0.5f);
-            fadeAnimator.SetBool("isFinished", true);
         }
-        else
+        else if (puzzleDict.TryGetValue(newState, out PuzzleSetup targetPuzzle))
         {
-            fadeAnimator.SetBool("isFinished", false);
-            fadeAnimator.SetTrigger("StartFade");
-            yield return new WaitForSeconds(1f);
-            //Turn Off/On camera 
+            // Disable player
             playerCamera.SetActive(false);
-            laserPuzzleCamera.SetActive(true);
-            yield return new WaitForSeconds(0.5f);
-            //Turn Off/On the gameobject 
             playerGameObject.SetActive(false);
-            laserPuzzleObject.SetActive(true);
-            yield return new WaitForSeconds(0.5f);
-            fadeAnimator.SetBool("isFinished", true);
-        }
-       
-    }
-    public void SetPlayerState(PlayerState state)
-    {
-        if (PlayerState.Moving == state)
-        {
-            StartCoroutine(SwitchCamera("Player"));
-        }
-        else if (PlayerState.LaserPuzzle == state) 
-        {
-            StartCoroutine(SwitchCamera("Puzzle"));
+            
+            // Disable current puzzle if there is one
+            if (currentActivePuzzle != null && currentActivePuzzle != targetPuzzle)
+            {
+                currentActivePuzzle.puzzleCamera.SetActive(false);
+                currentActivePuzzle.puzzleObject.SetActive(false);
+            }
+            
+            // Enable target puzzle
+            yield return new WaitForSeconds(transitionDuration);
+            targetPuzzle.puzzleCamera.SetActive(true);
+            targetPuzzle.puzzleObject.SetActive(true);
+            
+            // Update current active puzzle
+            currentActivePuzzle = targetPuzzle;
         }
         
+        // Complete the transition
+        yield return new WaitForSeconds(transitionDuration);
+        fadeAnimator.SetBool("isFinished", true);
+        
+        // Update current state
+        currentState = newState;
+    }
+
+    public void SetPlayerState(PlayerState state)
+    {
+        if (currentState != state)
+        {
+            StartCoroutine(TransitionToState(state));
+        }
     }
 }
